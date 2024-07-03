964ce7fb393a2ff51efce0f805159e09c420badd
anavictoriaacu@gmail.com <anavictoria1994>
2024-07-03 17:49:25 -0500

    mensaje de confirmación

---
+++ src/components/TodoComputed.jsx
@@ -0,0 +1,11 @@
+
+const TodoComputed = ({countItem, clearCompleted}) => { 
+    return(
+        <section className="flex justify-between bg-white py-4 px-4 rounded-b-md ">
+            <span className="text-gray-400"> {countItem} items left</span>
+            <button className="text-gray-400" onClick={clearCompleted} > Clear Completed</button>
+        </section>
+    )
+ }
+
+export default TodoComputed;
---
+++ src/components/TodoCreate.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const TodoCreate = ({createTodo}) => {
+
+  const [title, setTitle] = useState('');
+  
+  const handleSubmitAddTodo = (e) => {
+    e.preventDefault();
+    //const title = e.target.elements.title.value.trim();
+    if(!title.trim()){
+      return setTitle("");
+    }
+    createTodo(title);
+    setTitle("");
+  };
+
+    return(
+        <form onSubmit={handleSubmitAddTodo} className="flex gap-4 overflow-hidden bg-white rounded-md py-3 items-center px-4 mt-8">
+          <span className="inline-block h-5 w-5 rounded-full border-2"></span>
+          <input 
+          type="text" 
+          className="w-full text-gray-400 outline-none"
+          placeholder="Create a new todo..."
+          value ={title}
+          onChange={(e) => setTitle(e.target.value)}
+          />
+        </form>
+    )
+}
+
+export default TodoCreate;
